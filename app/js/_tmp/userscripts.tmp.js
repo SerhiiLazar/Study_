350811b093f41da6d627cdcf1cf034c19c18bd28
Serhii_Lazarenok <69537144+SerhiiLazar@users.noreply.github.com>
2020-11-26 00:37:39 +0200

    about_block

--- app/js/_tmp/userscripts.tmp.js
+++ app/js/_tmp/userscripts.tmp.js
@@ -2,3 +2,11 @@
 
 document.addEventListener('DOMContentLoaded', function () {// Custom JS
 });
+$(function () {
+  $('#boostup-slider__wrap').lightSlider({
+    adaptiveHeight: true,
+    item: 2,
+    slideMargin: 0,
+    loop: true
+  });
+});
--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 
 })
+$(function() {
+    $('#boostup-slider__wrap').lightSlider({
+        adaptiveHeight:true,
+        item:2,
+        slideMargin:0,
+        loop:true
+    });
+});
 
