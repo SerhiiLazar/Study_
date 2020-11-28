e520cc9fa67eff0e8954917b2584ec403375d008
Serhii_Lazarenok <69537144+SerhiiLazar@users.noreply.github.com>
2020-11-28 18:23:08 +0200

    sliserpractic

--- app/js/_tmp/userscripts.tmp.js
+++ app/js/_tmp/userscripts.tmp.js
@@ -2,11 +2,40 @@
 
 document.addEventListener('DOMContentLoaded', function () {// Custom JS
 });
-$(function () {
-  $('#boostup-slider__wrap').lightSlider({
-    adaptiveHeight: true,
-    item: 2,
-    slideMargin: 0,
-    loop: true
-  });
-});
+var line = document.querySelector('.line');
+var slides = document.querySelectorAll('.slide');
+var sliderWidth = document.querySelector('.slider').offsetWidth;
+var widthArray = [0];
+var lineWidth = 0;
+var offset = 0;
+var step = 0;
+var rest = 0;
+
+for (var i = 0; i < slides.length; i++) {
+  widthArray.push(slides[i].offsetWidth);
+  lineWidth += slides[i].offsetWidth;
+}
+
+line.style.width = lineWidth + 'px';
+console.log(widthArray);
+
+document.onclick = function () {
+  rest = lineWidth - sliderWidth - (offset + widthArray[step]);
+
+  if (rest >= 0) {
+    offset = offset + widthArray[step];
+    line.style.left = -offset + 'px';
+    step++;
+  } else {
+    line.style.left = -(lineWidth - sliderWidth) + 'px';
+    offset = 0;
+    step = -1;
+  }
+
+  if (step + 1 == slides.length) {
+    step = 0;
+    offset = 0;
+  } else {
+    step++;
+  }
+};
--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,39 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 
 })
-$(function() {
-    $('#boostup-slider__wrap').lightSlider({
-        adaptiveHeight:true,
-        item:2,
-        slideMargin:0,
-        loop:true
-    });
-});
+let line = document.querySelector('.line');
+let slides = document.querySelectorAll('.slide');
+let sliderWidth = document.querySelector('.slider') .offsetWidth;
+let widthArray = [0];
+let lineWidth = 0;
+let offset = 0;
+let step = 0;
+let rest = 0;
 
+for (let i = 0; i < slides.length; i++) {
+    widthArray.push(slides[i].offsetWidth);
+    lineWidth += slides[i].offsetWidth;
+}
+line.style.width = lineWidth+'px';
+console.log(widthArray);
+
+document.onclick = function () {
+    rest = lineWidth - sliderWidth - (offset + widthArray[step]);
+    if (rest >= 0) {
+        offset = offset+widthArray[step];
+        line.style.left = -offset+'px';
+        step++;
+    }
+    else {
+        line.style.left = -(lineWidth - sliderWidth) + 'px';
+        offset = 0;
+        step = -1;
+    }
+    if (step + 1 == slides.length) {
+        step = 0;
+        offset = 0;
+    }
+    else {
+        step++;
+    }
+}
